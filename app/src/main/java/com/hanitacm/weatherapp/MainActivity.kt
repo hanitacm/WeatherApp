459730cf2b63c9f164753ed0cebfd7abc6fcb29d
Hanitacm <ana.cordobam@gmail.com>
2018-12-02 11:41:38 +0800

    add mainActivityPresenter

--- app/src/main/java/com/hanitacm/weatherapp/MainActivity.kt
+++ app/src/main/java/com/hanitacm/weatherapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.hanitacm.weatherapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.sync
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var presenter: mainActivityPresenter
+
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    presenter = mainActivityPresenter()
+    sync.setOnClickListener { presenter.getWeatherSync() }
   }
+
+
 }
---
+++ app/src/main/java/com/hanitacm/weatherapp/mainActivityPresenter.kt
@@ -0,0 +1,8 @@
+package com.hanitacm.weatherapp
+
+class mainActivityPresenter {
+  fun getWeatherSync() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+}
