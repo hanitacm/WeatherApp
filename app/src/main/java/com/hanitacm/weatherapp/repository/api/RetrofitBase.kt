3e43f5b0c6713a0364a770928823dad144933004
Hanitacm <ana.cordobam@gmail.com>
2019-08-08 18:40:29 +0700

    extract retrofit creation to new class

--- app/src/main/java/com/hanitacm/weatherapp/repository/api/RetrofitBase.kt
+++ app/src/main/java/com/hanitacm/weatherapp/repository/api/RetrofitBase.kt
@@ -1,4 +1,17 @@
 package com.hanitacm.weatherapp.repository.api
 
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
 class RetrofitBase {
+  private val weatherAPIUrl = "http://api.openweathermap.org/data/2.5/"
+
+  fun configRetrofitCall():Retrofit = Retrofit.Builder()
+        .baseUrl(weatherAPIUrl)
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+
 }
--- app/src/main/java/com/hanitacm/weatherapp/repository/api/WeatherApi.kt
+++ app/src/main/java/com/hanitacm/weatherapp/repository/api/WeatherApi.kt
@@ -2,24 +2,17 @@ package com.hanitacm.weatherapp.repository.api
 
 import com.hanitacm.weatherapp.repository.data.WeatherData
 import io.reactivex.Single
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 
-class WeatherApi {
-  private val weatherAPIUrl = "http://api.openweathermap.org/data/2.5/"
+class WeatherApi(private val retrofit: RetrofitBase) {
   private val apiKey = "f33ec8d7f07ee949745b53d8474dbeb2"
 
+
   fun getWeather(location: String): Single<WeatherData> {
-    val retrofit = Retrofit.Builder()
-        .baseUrl(weatherAPIUrl)
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
 
-    val service = retrofit.create(WeatherService::class.java)
+    val weatherService = retrofit.configRetrofitCall()
+        .create(WeatherService::class.java)
 
-    return service.getWeatherData(apiKey, location)
+    return weatherService.getWeatherData(apiKey, location)
   }
 
 
