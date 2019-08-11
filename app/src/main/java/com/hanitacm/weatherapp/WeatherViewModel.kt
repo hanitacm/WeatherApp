4a678cce2ddd454b97c2a8502ce87801dd819354
Hanitacm <ana.cordobam@gmail.com>
2019-08-11 15:55:22 +0700

    fix error to get weather data

--- app/src/main/java/com/hanitacm/weatherapp/WeatherViewModel.kt
+++ app/src/main/java/com/hanitacm/weatherapp/WeatherViewModel.kt
@@ -4,7 +4,9 @@ import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
+import android.util.Log
 import com.hanitacm.weatherapp.domain.GetWeatherUseCase
+import com.hanitacm.weatherapp.domain.WeatherDomainModel
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
@@ -24,11 +26,20 @@ class WeatherViewModel(private val getWeatherUseCase: GetWeatherUseCase) : ViewM
       subscription.add(getWeatherUseCase.getWeather(location)
           .observeOn(AndroidSchedulers.mainThread())
           .subscribeOn(Schedulers.io())
-          .subscribe({ result -> weather.postValue(DisplayableWeather(result.description, result.temperature, result.humidity)) }
-              , { error -> })
+          .subscribe({ result -> processResponse(result) }
+              , { error -> showError(error) })
       )
 
     }
+  }
+
+  private fun showError(error: Throwable?) {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  private fun processResponse(result: WeatherDomainModel) {
+    Log.d("error", result.toString())
+    weather.postValue(DisplayableWeather(result.description, result.temperature, result.humidity))
   }
 
   override fun onCleared() {
--- app/src/main/java/com/hanitacm/weatherapp/repository/WeatherRepository.kt
+++ app/src/main/java/com/hanitacm/weatherapp/repository/WeatherRepository.kt
@@ -6,7 +6,8 @@ import io.reactivex.Single
 
 class WeatherRepository(private val weatherApi: WeatherApi) {
   fun getWeather(location: String): Single<WeatherDomainModel> {
-    return weatherApi.getWeather(location).map { WeatherDomainModel(it.weather.main, it.weather.description, it.main.temp, it.main.humidity)
+    return weatherApi.getWeather(location).map {
+      WeatherDomainModel(it.weather[0].main, it.weather[0].description, it.main.temp, it.main.humidity)
     }
   }
 
