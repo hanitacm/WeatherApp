e75b1d35e339318656ec91b3adc0a0e1a16a1216
Hanitacm <ana.cordobam@gmail.com>
2019-08-11 15:56:48 +0700

    delete weatherState

--- app/src/main/java/com/hanitacm/weatherapp/WeatherState.kt
+++
@@ -1,7 +0,0 @@
-package com.hanitacm.weatherapp
-
-data class WeatherState(
-    val weatherDataResult: DisplayableWeather,
-    val searchInProgress: Boolean = false,
-    val errorFounded: Boolean = false
-)
