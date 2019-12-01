0dca19337cc9b7dc9c0810e020df7734087fb410
Hanitacm <ana.cordobam@gmail.com>
2019-12-01 13:36:32 +0800

    add call api to get locations

--- app/src/main/java/com/hanitacm/weatherapp/repository/api/WeatherService.kt
+++ app/src/main/java/com/hanitacm/weatherapp/repository/api/WeatherService.kt
@@ -1,7 +1,6 @@
 package com.hanitacm.weatherapp.repository.api
 
 import com.hanitacm.weatherapp.repository.data.WeatherData
-import io.reactivex.Observable
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -10,5 +9,8 @@ import retrofit2.http.Query
 interface WeatherService {
 
   @GET("weather")
-  fun getWeatherData(@Query("APPID")api_key: String, @Query("q") location: String): Single<WeatherData>
+  fun getWeatherData(@Query("APPID") api_key: String, @Query("q") location: String): Single<WeatherData>
+
+  @GET("find?type=like&sort=population&cnt=30")
+  fun getWeatherLocation(@Query("APPID") api_key: String, @Query("q") location: String)
 }
