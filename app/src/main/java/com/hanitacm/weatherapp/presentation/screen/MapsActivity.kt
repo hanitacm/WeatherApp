1a7e3dcbe4ca567aac1c6c457ef3dd9ec4eb4b3e
Hanitacm <ana.cordobam@gmail.com>
2020-05-06 18:21:58 +0200

    change tabs text

--- app/src/main/java/com/hanitacm/weatherapp/presentation/screen/MapsActivity.kt
+++ app/src/main/java/com/hanitacm/weatherapp/presentation/screen/MapsActivity.kt
@@ -1,13 +1,9 @@
 package com.hanitacm.weatherapp.presentation.screen
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.hanitacm.weatherapp.R
 import com.hanitacm.weatherapp.presentation.screen.ui.main.SectionsPagerAdapter
 
@@ -21,11 +17,7 @@ class MapsActivity : AppCompatActivity() {
     viewPager.adapter = sectionsPagerAdapter
     val tabs: TabLayout = findViewById(R.id.tabs)
     tabs.setupWithViewPager(viewPager)
-    val fab: FloatingActionButton = findViewById(R.id.fab)
 
-    fab.setOnClickListener { view ->
-      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-          .setAction("Action", null).show()
-    }
+
   }
 }
