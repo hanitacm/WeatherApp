a2ae4812ad8fa3cd14d1b4b274d8d3612d71f016
Hanitacm <ana.cordobam@gmail.com>
2020-06-05 20:25:33 +0200

    add userLocationRepository unit test

--- app/src/main/java/com/hanitacm/weatherapp/repository/UserLocationRepository.kt
+++ app/src/main/java/com/hanitacm/weatherapp/repository/UserLocationRepository.kt
@@ -1,41 +1,58 @@
 package com.hanitacm.weatherapp.repository
 
-import android.location.Location
 import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationAvailability
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
+import com.hanitacm.weatherapp.domain.UserLocationDomainModel
+import com.hanitacm.weatherapp.repository.data.mapper.UserLocationDomainMapper
 import io.reactivex.Single
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class UserLocationRepository @Inject constructor(private val fusedLocationProviderClient: FusedLocationProviderClient) {
+class UserLocationRepository @Inject constructor(private val fusedLocationProviderClient: FusedLocationProviderClient, private val mapper: UserLocationDomainMapper) {
 
-  fun getUserLocation(): Single<Location> {
-
+  fun getUserLocation(): Single<UserLocationDomainModel> {
     return Single.create { emitter ->
-
-      val locationRequest = LocationRequest().apply {
-        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        interval = TimeUnit.SECONDS.toMillis(10)
-      }
-
-      val locationCallback = object : LocationCallback() {
-        override fun onLocationResult(result: LocationResult?) {
-          super.onLocationResult(result)
-          result?.let {
-            emitter.onSuccess(it.lastLocation)
-
-          }
+      fusedLocationProviderClient.lastLocation.addOnSuccessListener { taskLocation ->
+        if (taskLocation != null) {
+          emitter.onSuccess(mapper.mapToDomainModel(taskLocation))
+        } else {
+          emitter.onError(IllegalStateException("Location is null"))
         }
-
       }
-      fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
-
     }
 
   }
+
+
+//fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
+//OR
+//      fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+//        location?.let {
+//          emitter.onSuccess(mapper.mapToDomainModel(it))
+//        }
+//            ?: emitter.onError(Exception())
+
+//      }
+// stop(locationCallback)
+
+// }
+//}
+// }
+
+  private fun createLocationRequest(): LocationRequest {
+    return LocationRequest().apply {
+      priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+      interval = TimeUnit.SECONDS.toMillis(10)
+    }
+  }
+
+  private fun stop(locationCallback: LocationCallback) {
+    fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+  }
+
+
 }
+
