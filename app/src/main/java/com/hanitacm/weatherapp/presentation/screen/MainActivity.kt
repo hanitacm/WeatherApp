2c605a8c20bf837becb4b2b4cd402cc56d4e6ce1
Hanitacm <ana.cordobam@gmail.com>
2020-08-12 13:48:57 +0200

    add floating search view

--- app/src/main/java/com/hanitacm/weatherapp/presentation/screen/MainActivity.kt
+++ app/src/main/java/com/hanitacm/weatherapp/presentation/screen/MainActivity.kt
@@ -3,16 +3,12 @@ package com.hanitacm.weatherapp.presentation.screen
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.hanitacm.weatherapp.R
-import kotlinx.android.synthetic.main.activity_main.toolbar
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
-    setSupportActionBar(toolbar)
-
-
   }
 
 }
