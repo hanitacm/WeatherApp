54815f4d32f0780c61b8eca3251ca394f9a350c9
Hanitacm <ana.cordobam@gmail.com>
2020-08-21 12:10:48 +0200

    edit UserLocationRepository.kt to extract LocationGoogleServiceProvider and edit unit test

--- app/src/test/java/com/hanitacm/weatherapp/repository/UserLocationRepositoryShould.kt
+++ app/src/test/java/com/hanitacm/weatherapp/repository/UserLocationRepositoryShould.kt
@@ -1,37 +1,25 @@
 package com.hanitacm.weatherapp.repository
 
 import android.location.Location
-import android.os.Build
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.tasks.OnSuccessListener
-import com.google.android.gms.tasks.Task
 import com.hanitacm.weatherapp.repository.data.mapper.UserLocationDomainMapper
+import com.hanitacm.weatherapp.repository.provider.LocationGoogleServiceProvider
 import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doAnswer
-import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.never
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
-import org.junit.Before
+import io.reactivex.Single
 import org.junit.Test
-import org.junit.jupiter.api.AfterEach
 import org.junit.runner.RunWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
-import org.mockito.Mockito.verify
-import org.mockito.MockitoAnnotations
+import org.mockito.Mockito
 import org.mockito.Spy
-import org.mockito.invocation.InvocationOnMock
-import org.robolectric.RobolectricTestRunner
-import org.robolectric.annotation.Config
+import org.mockito.junit.MockitoJUnitRunner
 
-
-@RunWith(RobolectricTestRunner::class)
-@Config(sdk = [Build.VERSION_CODES.P])
-internal class UserLocationRepositoryShould {
+@RunWith(MockitoJUnitRunner::class)
+class UserLocationRepositoryShould {
   @Mock
-  private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
-
-  @Mock
-  private lateinit var mockTask: Task<Location>
+  private lateinit var locationGoogleServiceProvider: LocationGoogleServiceProvider
 
   @Spy
   private lateinit var mapper: UserLocationDomainMapper
@@ -39,11 +27,6 @@ internal class UserLocationRepositoryShould {
   @InjectMocks
   private lateinit var userLocationRepository: UserLocationRepository
 
-  @Before
-  fun setUp() {
-    MockitoAnnotations.initMocks(this)
-
-  }
 
   @Test
   fun `return last valid location`() {
@@ -53,8 +36,7 @@ internal class UserLocationRepositoryShould {
 
 
 
-    whenever(fusedLocationProviderClient.lastLocation).thenReturn(mockTask)
-    `when location provider returns a response`(location)
+    whenever(locationGoogleServiceProvider.requestLocation()).thenReturn(Single.just(location))
     whenever(mapper.mapToDomainModel(location)).thenCallRealMethod()
 
 
@@ -65,8 +47,8 @@ internal class UserLocationRepositoryShould {
     locationResult.assertNoErrors()
 
 
-    verify(fusedLocationProviderClient).lastLocation
-    verify(mapper).mapToDomainModel(location)
+    Mockito.verify(locationGoogleServiceProvider).requestLocation()
+    Mockito.verify(mapper).mapToDomainModel(location)
 
     locationResult.assertValue { it.longitude == location.longitude && it.latitude == location.latitude }
 
@@ -74,8 +56,7 @@ internal class UserLocationRepositoryShould {
 
   @Test
   fun `throw an exception when provider returns a null value as location`() {
-    whenever(fusedLocationProviderClient.lastLocation).thenReturn(mockTask)
-    `when location provider returns a response`(null)
+    whenever(locationGoogleServiceProvider.requestLocation()).thenReturn(Single.error(IllegalStateException("Location is null")))
 
     val locationResult = userLocationRepository.getUserLocation().test()
 
@@ -83,28 +64,15 @@ internal class UserLocationRepositoryShould {
     locationResult.assertError(IllegalStateException::class.java)
 
 
-    verify(fusedLocationProviderClient).lastLocation
-    verify(mapper, times(0))
+    verify(locationGoogleServiceProvider).requestLocation()
+    verify(mapper, never()).mapToDomainModel(any())
 
 
-  }
-
-  private fun `when location provider returns a response`(location: Location?) {
-    doAnswer { invocation: InvocationOnMock? ->
-      val arguments = invocation?.arguments
-      val listener = arguments?.get(0) as OnSuccessListener<Location>
-      listener.onSuccess(location)
-      null
-    }.`when`(mockTask).addOnSuccessListener(any())
   }
 
   private fun `given a location from gps`(): Location = Location("test_provider").apply {
     latitude = 40.0
     longitude = 3.0
     time = System.currentTimeMillis()
-  }
-
-  @AfterEach
-  fun tearDown() {
   }
 }
