972fb7f5b5e4686369be2864c32969800a7c5435
Hanitacm <ana.cordobam@gmail.com>
2020-09-03 13:10:34 +0200

    add getForecastUseCase

--- app/src/main/java/com/hanitacm/weatherapp/domain/usecase/GetForecastUseCase.kt
+++ app/src/main/java/com/hanitacm/weatherapp/domain/usecase/GetForecastUseCase.kt
@@ -1,12 +1,17 @@
 package com.hanitacm.weatherapp.domain.usecase
 
+import com.hanitacm.weatherapp.domain.ErrorHandler
+import com.hanitacm.weatherapp.domain.Result
+import com.hanitacm.weatherapp.domain.extensions.toResult
 import com.hanitacm.weatherapp.domain.model.UserLocationDomainModel
+import com.hanitacm.weatherapp.domain.model.WeatherDomainModel
 import com.hanitacm.weatherapp.repository.WeatherRepository
+import io.reactivex.Single
 import javax.inject.Inject
 
-class GetForecastUseCase @Inject constructor(weatherRepository: WeatherRepository) {
+class GetForecastUseCase @Inject constructor(private val weatherRepository: WeatherRepository, private val errorHandler: ErrorHandler) {
 
-  fun getForecast(location: UserLocationDomainModel) {
-    //weatherRepository.
+  fun getForecast(location: UserLocationDomainModel): Single<Result<List<WeatherDomainModel>>> {
+    return weatherRepository.getWeatherForecast(location).toResult(errorHandler)
   }
 }
